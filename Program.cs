00fb51c0105ed7602ee7702bfb5a8cef9602e675
niles <niles@LAPTOP-3K24CQ1R>
2021-09-08 22:43:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleAppExtensionMethodOrderBy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 10, 45, 15, 39, 21, 26 };
+            var result = ints.OrderBy(g => g);
+            Console.WriteLine("Element of array in ascending order : ");
+            foreach(var i in result)
+            {
+                System.Console.Write(i + " ");
+            }
+            
+        }
+    }
+}
